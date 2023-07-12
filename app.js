b509a776104d932a0248083acdbb4e14ac7ac7ac
alroissul <alroissul@gmail.com>
2023-07-13 01:00:58 +0700

    todolist v2 add connection to database and dynamic routing

--- app.js
+++ app.js
@@ -1,17 +1,75 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 const port = 3000;
-var items = ["Going to gym", "finish course for website"];
-let workItems = [];
+
 // App that's going to be used --Opening
 // app.use(express.static("public")); 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static("public"));
 
+//Go live & Set up Database 
+mongoose.connect("mongodb://127.0.0.1:27017/toDolistProject");
+
+const taskSchema = mongoose.Schema({
+    activity : String,
+    time : Number,
+    priority : String,
+    notes : String,
+    working : Boolean,
+    pomoCountActual : Number
+});
+
+const timerSchema = mongoose.Schema({
+    pomoFocusTime : String,
+    shortbreakTime : Number,
+    longBreakTime : String,
+    notes : String
+});
+
+const listsSchema = mongoose.Schema({
+    listName : String,
+    activity : [taskSchema]
+});
+
+const tasks = mongoose.model("tasks",taskSchema);
+const timer = mongoose.model("timer", timerSchema);
+const lists = mongoose.model("lists",listsSchema);
+
+
+const howToDelete = new tasks({
+    activity: "<-- Click Here to Delete a task",
+    priority: "urgent-important"
+});
+
+const howToAdd = new tasks({
+    activity: "Type below to add new item !",
+    priority : "urgent-important"
+});
+
+const defaultTask = [howToDelete,howToAdd];
+
+// Add default value if database hasn't existed yet
+tasks.find().then(async function(task){
+    if (task.length === 0) {
+        tasks.insertMany(defaultTask).then((insertedTask,err)=>{
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Default Items has been sucessfully added");
+            };
+        });
+    };
+});
+
+
 // App that's going to be used --Closing
+
+
 // get requests --Opening
 app.get("/", function(req, res ){
     const currentDate = new Date();
@@ -24,28 +82,80 @@ app.get("/", function(req, res ){
         typeOfDay = "it's the weekend !";
     } else {
         typeOfDay = "it's the weekdays !";
-    }; 
-    // res.sendFile(__dirname+"/index.html");
-    // res.render("list2", {day:day , typeOfDay:typeOfDay,items:items});
-    res.render("list", {listTitle:day, typeOfDay,items:items});
+    };
+
+    tasks.find().then(async function(task,err){
+        if (err){
+            console.log(err);
+        } else {
+            res.render("list", {listTitle:"Today", typeOfDay,items:task});
+        };
+    });
+});
+        
+// Routing for other lists            
+app.get("/:userList", async (req, res)=> {    
+    const customListName = _.capitalize(req.params.userList);
+    const currentList = await lists.findOne({listName: customListName});
+
+    if (!currentList) {
+        const list = new lists({
+            listName : customListName,
+            activity : defaultTask  
+        });
+        list.save();
+        res.redirect(`/${list.listName}`)
+    } else {
+        res.render("list",{listTitle:currentList.listName, items:currentList.activity})
+    };
 });
 
-app.get("/work", function(req, res) {
-    res.render("list",{listTitle:"Work List", items: workItems});
-});
+
 // get requests -- Closing
 
 // post requests -- Opening
 app.post("/", function(req, res) {
-    var newItem = req.body.itemName;
-    items.push(newItem);
-    res.redirect("/");
+    const newItem = new tasks({
+        activity : req.body.itemName,
+        priority : "urgent-important"
+    });
+
+    const listName = req.body.list; 
+    //adding it to the database this is dynamic routing so also
+    if (_.lowerCase(listName) === "today" ) {
+        newItem.save();
+        res.redirect("/");
+    } else {
+        lists.findOne({listName :listName}).then((foundList, err)=> { // find & return the list 
+        foundList.activity.push(newItem); // it will save newly created task and append it to the array of tasked for the current list
+        foundList.save(); // this will save it 
+        res.redirect(`/${listName}`); // this will redirect it to the correct route
+        });
+    };
+
 });
 
-app.post("/work", function(req, res) {
-    workItems.push(req.body.itemName);
-    res.redirect("/work");
-})
+app.post("/delete", function(req, res){
+    const removeTask = req.body.check;
+    const listName = req.body.listName;
+
+    tasks.findByIdAndRemove(removeTask).then((task,err) => {
+        if (_.lowerCase(listName) === "today" ) {
+            if (!err) {
+                res.redirect("/");
+            } else {
+                console.log(err);
+            };
+        } else {
+            lists.findOneAndUpdate({listName : listName},{$pull:{activity:{_id:removeTask}}}).then((removedList,err)=>{
+                res.redirect(`/${listName}`);
+            });
+        };
+        
+    });
+});
+
+// Post requests -- Closing
 
 app.listen(port,function(){
     console.log(`Listening to port ${port}`);
